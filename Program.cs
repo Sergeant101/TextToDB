cc7f2b734a8a27e7c13b3dd733f563d7c7a31d0d
Sergeant101 <s.v.zagorodnev@gmail.com>
2021-10-01 13:45:03 +0500

    <first commit>

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextToDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
